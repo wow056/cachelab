4da9f610286c27f7a7e9c7f267891ec4dcb09532
Minsu_Kang <powerful@postech.ac.kr>
2018-11-13 17:35:48 +0900

    ah....

--- csim.c
+++ csim.c
@@ -9,6 +9,7 @@
 typedef struct {
     int valid;
     int tag;
+    unsigned long used_timing;
 } line;
 
 typedef struct {
@@ -19,44 +20,60 @@ typedef struct {
     int set_num;
     int line_num;
     int block_bytes;
+    unsigned long instruction_count;
     set* sets;
 } cache;
 
-int parse_block_offset(long address, int b);
-int parse_set_index(long address, int s, int b);
-int parse_tag(long address, int s, int b);
+int s, b;
+int hit_count, miss_count, eviction_count;
+
+cache* allocate_cache(int set_num, int line_num, int block_bytes);
+void deallocate_cache(cache* c);
+int find_cache(cache* c, int address);
+
+int parse_block_offset(unsigned long address);
+int parse_set_index(unsigned long address);
+int parse_tag(unsigned long address);
 
 int parse_argument(int argc, char *argv[], int *vflag, int *set_bit, int *lines_per_set, int *block_bit, FILE **tracefile);
-int parse_memory_trace(const char *str, long *address, int *size);
+int parse_memory_trace(const char *str,char *instruction, unsigned long *address, int *size);
+
+int match_line_address(line l, unsigned long address);
+void fetch(cache* c, set* current_set, unsigned long address);
 
 int main(int argc, char *argv[])
 {
-    int verbose_flag, set_bit, line_num, block_bit_num;
+    int verbose_flag, line_num;
     int set_num,block_num;
+    char instruction;
     int memsize;
-    long address;
+    unsigned long address;
     FILE *f;
     char buffer[100];
     int block_offset, set_index, tag;
+    cache *c;
 
-    if(parse_argument(argc, argv, &verbose_flag, &set_bit, &line_num, &block_bit_num, &f))
+    if(parse_argument(argc, argv, &verbose_flag, &s, &line_num, &b, &f))
     {
-        set_num = 1 << set_bit;
-        block_num = 1 << block_bit_num;
+        set_num = 1 << s;
+        block_num = 1 << b;
         printf("v:%d, s:%d, E:%d, b:%d\n",verbose_flag, set_num, line_num, block_num);
+        c = allocate_cache(set_num, line_num, block_num);
         while(fgets(buffer, 100, f))
         {
-            if(parse_memory_trace(buffer, &address, &memsize))
+            if(parse_memory_trace(buffer, &instruction, &address, &memsize))
             {
-                printf("%s",buffer);
-                printf("address: 0x%lx, memsize: %d\n", address, memsize);
-                tag = parse_tag(address, set_bit, block_bit_num);
-                set_index = parse_set_index(address, set_bit, block_bit_num);
-                block_offset = parse_block_offset(address, set_bit);
-                printf("tag: 0x%x, set_index: 0x%x, block_offset: 0x%x\n\n", tag, set_index, block_offset);
+                //printf("%s",buffer);
+                //printf("instruction: %c, address: 0x%lx, memsize: %d\n",instruction, address, memsize);
+                cache_find_iteration(c, instruction, address);
+                //tag = parse_tag(address);
+                //set_index = parse_set_index(address);
+                //block_offset = parse_block_offset(address);
+                //printf("tag: 0x%x, set_index: 0x%x, block_offset: 0x%x\n\n", tag, set_index, block_offset);
             }
         }
         printSummary(0, 0, 0);
+        deallocate_cache(c);
         return 0;
     }
     else
@@ -65,17 +82,19 @@ int main(int argc, char *argv[])
     }
 }
 
-int parse_memory_trace(const char *str,long *address, int *size)
+int parse_memory_trace(const char *str,char *instruction, unsigned long *address, int *size)
 {
     int size_temp;
-    long address_temp;
+    unsigned long address_temp;
+    char buff[5];
     if(str[0] == 'I')
     {
         return 0;
     }
     else
     {
-        sscanf(str,"%*s%lx,%d",&address_temp, &size_temp);
+        sscanf(str,"%s%lx,%d",buff,&address_temp, &size_temp);
+        *instruction = buff[0];
         *address = address_temp;
         *size = size_temp;
         return 1;
@@ -91,14 +110,20 @@ int parse_argument(int argc, char *argv[], int *vflag, int *set_bit, int *lines_
     {
         switch(opt)
         {
-            case 'h':
             default:
-                printf("-h: Optional help flag that prints usage info\n");
-                printf("-v: Optional verbose flag that displays trace info\n");
-                printf("-s <s>: Number of set index bits (S = 2s is the number of set)\n");
-                printf("-E <E>: Associativity (number of lines per set)\n");
-                printf("-b <b>: Number of block bits (B = 2b is the block size)\n");
-                printf("-t <tracefile>: Name of the valgrind trace to replay");
+                break;
+            case 'h':
+                printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
+                printf("Options:\n");
+                printf(" -h         Print this help message.\n");
+                printf(" -v         Optional verbose flag.\n");
+                printf(" -s <num>   Number of set index bits.\n");
+                printf(" -E <num>   Number of lines per set.\n");
+                printf(" -b <num>   Number of block offset bits.\n");
+                printf(" -t <file>  Trace file.\n\n");
+                printf("Examples:\n");
+                printf("  linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n", argv[0]);
+                printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
                 break;
             case 'v':
                 vflag_temp = 1;
@@ -128,24 +153,140 @@ int parse_argument(int argc, char *argv[], int *vflag, int *set_bit, int *lines_
     }
     else
     {
+        printf(" %s: Missing required command line argument\n", argv[0]);
+        printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
+        printf("Options:\n");
+        printf(" -h         Print this help message.\n");
+        printf(" -v         Optional verbose flag.\n");
+        printf(" -s <num>   Number of set index bits.\n");
+        printf(" -E <num>   Number of lines per set.\n");
+        printf(" -b <num>   Number of block offset bits.\n");
+        printf(" -t <file>  Trace file.\n\n");
+        printf("Examples:\n");
+        printf("  linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n", argv[0]);
+        printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
         return 0;
     }
 }
 
-int parse_block_offset(long address, int b)
+int parse_block_offset(unsigned long address)
 {
     return address & ((0x1 << b) - 1); 
 }
 
-int parse_set_index(long address, int s, int b)
+int parse_set_index(unsigned long address)
 {
     address >>= b;
     return address & ((0x1 << s) - 1);
 }
-int parse_tag(long address, int s, int b)
+int parse_tag(unsigned long address)
 {
     unsigned long temp = address; 
     return (int)((unsigned long)temp >> (s + b));
 }
 
+
+cache* allocate_cache(int set_num, int line_num, int block_bytes)
+{
+    cache *temp; 
+    int i, j;
+
+    temp = (cache*)malloc(sizeof(cache));
+    temp->set_num = set_num;
+    temp->line_num = line_num;
+    temp->block_bytes = block_bytes;
+    temp->sets = (set*)malloc(sizeof(set) * temp->set_num);
+    temp->instruction_count = 0;
+    for(i = 0; i < set_num; i++)
+    {
+        temp->sets[i].lines = (line*)malloc(sizeof(line) * temp->line_num); 
+        for(j=0; j<temp->line_num; j++)
+        {
+            temp->sets[i].lines[j].valid = 0;
+            temp->sets[i].lines[j].tag = 0;
+            temp->sets[i].lines[j].used_timing = 0;
+        }
+    }
+    return temp;
+}
+
+void deallocate_cache(cache* c)
+{
+    int i;
+    for(i=0;i<c->set_num; i++){
+        free(c->sets[i].lines);
+    }
+    free(c);
+}
+
+int match_line_address(line l, unsigned long address)
+{
+    return (parse_tag(address) == l.tag) && l.valid; 
+}
+
+void cache_find_iteration(cache *c, char instruction, unsigned long address)
+{
+    int i;
+    set* current_set;
+    unsigned long min_timing_val;
+    int oldest_index;
+    int is_miss= 0, is_hit = 0, is_eviction;
+    current_set = &(c->sets[parse_set_index(address)]);
+    for(i=0; i<c->line_num;i++)
+    {
+        if(match_line_address(current_set->lines[i], address))
+        {
+            //hit
+            current_set->lines[i].used_timing = c->instruction_count;
+            is_hit = 1;
+            break;
+        }
+    }
+    
+    //if no match
+    if(i == c->line_num)
+    {
+        //find empty string
+        for(i = 0;i<c->line_num; i++)
+        {
+
+        }
+        min_timing_val = c->instruction_count;
+        //find LRU
+        for(i=0; i<c->line_num;i++)
+        {
+            if(current_set->lines[i].used_timing < min_timing_val)
+            {
+                min_timing_val = current_set->lines[i].used_timing;
+                oldest_index = i;
+            }
+        }
+        if(i == c->line_num)
+        {
+            fetch(c, current_set, address);
+            for(i=0; i < c->line_num; i++)
+            {
+                if(!current_set->lines[i].valid)
+                {
+                    current_set->lines[i].tag = parse_tag(address);
+                    printf("hit"); 
+                    break;
+                }
+            }
+        }
+    }
+    if(is_hit)
+    {
+
+    }
+    else if(is_miss)
+    {
+
+    }
+    else if(is_eviction)
+    {
+
+    }
+    c->instruction_count++;
+}
 
